e421385f7fb95b21691766a52f813510f562903d
Franck Koukal <47780261+franckkoukal@users.noreply.github.com>
2019-07-30 22:49:10 +0200

    Add files via upload

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://heroku_2xmwb1gl:uhtsa1qsblt9al1ugqe9qh1kvf@ds237922.mlab.com:37922/heroku_2xmwb1gl',
+  googleClientID:'971393794622-ss9p2aolk1r734i00icoquogj7orc0k8.apps.googleusercontent.com',
+  googleClientSecret:'971393794622-ss9p2aolk1r734i00icoquogj7orc0k8.apps.googleusercontent.com'
+}
